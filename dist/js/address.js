028b125c71cc16e31c68992296dbc3440c8dea15
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-06-25 17:25:19 +0330

    commit message

--- dist/js/address.js
+++ dist/js/address.js
@@ -15,21 +15,3 @@ overlay.addEventListener('click', function () {
     editAddress.classList.remove('active');
     overlay.classList.remove('active');
 })
-/////edit map
-let editMapBtn = document.getElementById('editMapBtn');
-let saveMapModal = document.getElementById('saveMapModal');
-let closeSaveMapModal = document.getElementById('closeSaveMapModal');
-let overlay2 = document.getElementById('overlay2');
-
-editMapBtn.addEventListener('click', function () {
-    saveMapModal.classList.add('active');
-    overlay2.classList.add('active');
-})
-closeSaveMapModal.addEventListener('click', function () {
-    saveMapModal.classList.remove('active');
-    overlay2.classList.remove('active');
-})
-overlay2.addEventListener('click', function () {
-    saveMapModal.classList.remove('active');
-    overlay2.classList.remove('active');
-})

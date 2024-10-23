eee38fdf337213a03cbded8311d9dd99c0e3da21
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-10-23 20:48:37 +0330

    commit message

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -427,3 +427,15 @@ showAllCat.addEventListener('click', function () {
     this.parentElement.style.display = 'none';
 })
 
+//initiate the plugin and pass the id of the div containing gallery images
+$('#zoom_03').ezPlus({
+    gallery: 'gallery_01', cursor: 'pointer', galleryActiveClass: 'active',
+    imageCrossfade: true, loadingIcon: 'http://www.elevateweb.co.uk/spinner.gif'
+});
+
+//pass the images to Fancybox
+$('#zoom_03').bind('click', function (e) {
+    var ez = $('#zoom_03').data('ezPlus');
+    $.fancyboxPlus(ez.getGalleryList());
+    return false;
+});
